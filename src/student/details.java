24f7bab2e7c8c2e31f257960549a1fcf510db050
supriya gondala <supriyagondala002@gmail.com>
2021-12-08 11:32:52 +0530

    this is my first commit

---
+++ src/student/details.java
@@ -0,0 +1,15 @@
+package student;
+
+public class details {
+	int id;
+	String name;
+	String branch;
+	public details(int id,String name,String branch) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.branch = branch;
+	}
+
+
+}
---
+++ src/student/main.java
@@ -0,0 +1,27 @@
+package student;
+
+public class main {
+public static void main(String[] args) {
+student s1=new student();
+s1.setId(45);
+s1.setName("supriya");
+s1.setBranch("cse");
+System.out.println(s1.getId());
+System.out.println(s1.getName());
+System.out.println(s1.getBranch());
+student s2=new student();
+s2.setId(1);
+s2.setName("mahi");
+s2.setBranch("cse");
+System.out.println(s2.getId());
+System.out.println(s2.getName());
+System.out.println(s2.getBranch());
+}
+
+
+
+
+
+
+
+}
